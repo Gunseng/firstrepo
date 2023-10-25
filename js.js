34460022b779b961eb2cfdc8f1925381de9e908f
unknown <gunsanseng24@gmail.com>
2023-10-25 10:54:07 +0630

    test

---
+++ js.js
@@ -0,0 +1,97 @@
+document.write("Hello my friend. How are you?");
+document.write("<br>")
+// alert("hello it is you own.")
+var name="Mg Mg is doctor";
+var about="This is a strin.😀";
+var h3="It's 6 o'clock🤫";
+alert (name);
+document.write(about);
+document.write("<br>");
+document.write(h3);
+document.write("<br>");
+var h4='Remember to say "please" and "thank you."';
+document.write(h4);
+document.write("<br>");
+document.write("How are you?".length);
+document.write("<br>");
+document.write("THE IS MY WORLD".toLowerCase());
+document.write("<br>");
+document.write("zaw zaw is driver".toUpperCase());
+document.write("<br>");
+document.write("    but keep the middle spaces   ".trim());
+document.write("<br>");
+document.write("hey what up man😁")
+document.write("<br>");
+var h1="hey man 😅"
+document.write(h1);
+document.write("<br>");
+// for number
+var num_one=36;
+var num_two=51.2563;
+var sum;
+sum= num_one+num_two;
+document.write("Total is:"+sum);
+document.write("<br>");
+var num_three= 85;
+var num_four=5.988451/7;
+sum= num_three-num_four;
+var sum;
+document.write(sum)
+document.write("<br>")
+var num_five= 2.78/435;
+var num_six=5.98/7/4.15;
+sum= num_three*num_four;
+var sum;
+document.write(sum)
+document.write("<br>")
+var num_seven= 2/4.589;
+var num_eight=5/7/2.756;
+sum= num_seven/num_eight;
+var sum;
+var a = 2;
+var b =a++;
+document.write("A"+a+"B"+b)
+function callname(){
+    return"Aye Aye";
+}
+var cn=callname();
+console.log(cn);
+function age(){
+    return 36;
+}
+var ae=age();
+console.log(ae);
+function add(none,ntwo,nthree,nfour){
+    return none*ntwo/nthree+nfour;
+}
+var res=add(9,5,8,5);
+console.log("answer"+res);
+
+function add2(number1,number2){
+    return number1/number2;
+}
+console.log("Ans"+add2(19,76));
+function cal(number3,number4){
+    return number3/number4;
+}
+const pi =3.142;
+function circle(rad){
+    var circle=pi*rad*rad;
+    return circle;
+}
+console.log("ans"+circle(25))
+function cal(numb1,numb2){
+    var cal=numb1*numb2;
+    return cal;
+}
+console.log("Ans"+cal(5,3))
+function cal8(n1,n2,n3,n4){
+    var cal8=n1*n2/n3*n4;
+    return cal8;
+}
+console.log("ans"+cal8(31,58,6,58))
+function cal5(nb1,nb2,nb3){
+    var cal5=nb1*nb2/nb3;
+    return cal5;
+}
+console.log("Ans"+cal5(8,5,6))
